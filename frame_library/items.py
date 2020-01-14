13036a6b0a3189b9cbe0c0a67cd2f5c1cb3307a7
fanyibin <f84041507@163.com>
2020-01-14 10:39:45 +0800

    update items

--- frame_library/items.py
+++ frame_library/items.py
@@ -7,31 +7,38 @@
 from frame_library.singleton import Singleton
 
 
-class NewsItem(Singleton):
+class NewsItem(metaclass=Singleton):
 
-    url = None,
-    title = None,
-    classify = None,
-    source = None,
-    web_source = None,
-    cover = None,  # str
-    content_text = None, # list
-    content_html = None,
-    images = None,  # list
-    publish_timestr = None,
-    publish_timestamp = None,  # int: ms level
-    keywords = None,  # list
-    article_type = None,  # int: 1->has no images; 2->has images
+    def __init__(self,
+                 url, title, source, web_source, cover, content_text, content_html, images,
+                 publish_timestr, publish_timestamp, keywords, article_type, classify):
+        self.url = url,
+        self.title = title,
+        # 新闻来源
+        self.source = source,
+        # 网页来源
+        self.web_source = web_source,
+        # 封面图片 ->str
+        self.cover = cover,
+        # 文章文本 ->list
+        self.content_text = content_text,
+        # 文章html ->str
+        self.content_html = content_html,
+        # 图片列表 ->list
+        self.images = images,
+        # 时间格式化字符串 ->str
+        self.publish_timestr = publish_timestr,
+        # 毫秒级时间戳 ->int
+        self.publish_timestamp = publish_timestamp,
+        # ->list
+        self.keywords = keywords,
+        # ->int 纯文字：1；含图片；2
+        self.article_type = article_type,
+        # china='国内', world='国际', society='社会', law='法制', ent='娱乐', tech='科技',
+        # life='生活', finance='财经', mil='军事', culture='文化',
+        self.classify = classify,
 
-    news_classify = dict(
-        china='国内',
-        world='国际',
-        society='社会',
-        law='法制',
-        ent='娱乐',
-        tech='科技',
-        life='生活',
-        finance='财经',
-        mil='军事',
-        culture='文化',
-    )
+
+if __name__ == '__main__':
+    pass
+
